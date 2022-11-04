1cb6423f3df4e03b18ea0565d2d6b9236b7cb26e
Olivér Falvai <oliver.falvai@bitrise.io>
2022-11-04 16:56:35 +0100

    Update go-steputils, update token generation

--- step/step.go
+++ step/step.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/bitrise-io/go-steputils/v2/cache"
 	"github.com/bitrise-io/go-steputils/v2/stepconf"
+	"github.com/bitrise-io/go-utils/v2/command"
 	"github.com/bitrise-io/go-utils/v2/env"
 	"github.com/bitrise-io/go-utils/v2/log"
 )
@@ -28,17 +29,20 @@ type RestoreCacheStep struct {
 	logger      log.Logger
 	inputParser stepconf.InputParser
 	envRepo     env.Repository
+	cmdFactory  command.Factory
 }
 
 func New(
 	logger log.Logger,
 	inputParser stepconf.InputParser,
 	envRepo env.Repository,
+	cmdFactory command.Factory,
 ) RestoreCacheStep {
 	return RestoreCacheStep{
 		logger:      logger,
 		inputParser: inputParser,
 		envRepo:     envRepo,
+		cmdFactory:  cmdFactory,
 	}
 }
 
@@ -55,7 +59,7 @@ func (step RestoreCacheStep) Run() error {
 
 	step.logger.EnableDebugLog(input.Verbose)
 
-	restorer := cache.NewRestorer(step.envRepo, step.logger)
+	restorer := cache.NewRestorer(step.envRepo, step.logger, step.cmdFactory)
 	return restorer.Restore(cache.RestoreCacheInput{
 		StepId:  stepId,
 		Verbose: input.Verbose,
